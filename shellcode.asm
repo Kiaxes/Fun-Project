9843068d5732649974bb5c958b6e24973da22780
Kiaxes <58788340+Kiaxes@users.noreply.github.com>
2020-11-20 10:33:36 -0600

    Update shellcode.asm

--- shellcode.asm
+++ shellcode.asm
@@ -2,6 +2,7 @@
 	; Assembly shellcode challenge
 	; 29 Byte solution
 	; Chirawat (Kim) Yooyuenyong
+	; Each set represent the change
 	
 	mov eax, 5
 	mov ebx, 0x08049b12
@@ -75,24 +76,24 @@
 	
 	=================
 	
-    mov    ebx,0x8049b12
-    xor    ecx,ecx
-    lea    eax, [ecx+0x05]
-    cdq
-    int    0x80
+	mov    ebx,0x8049b12
+	xor    ecx,ecx
+	lea    eax, [ecx+0x05]
+	cdq
+	int    0x80
 
-    mov    ecx ,ebx
-    mov    ebx,eax
-    lea    eax, [edx+0x03]
-    add    dl,0x1e
-    int    0x80
+	mov    ecx ,ebx
+	mov    ebx,eax
+	lea    eax, [edx+0x03]
+	add    dl,0x1e
+	int    0x80
 
 
 	lea    eax, [edx - 0x1A]
-    lea    ebx, [eax-0x03]
-    cdq
-    add    dl,0x1e
-    int    0x80
+	lea    ebx, [eax-0x03]
+	cdq
+	add    dl,0x1e
+	int    0x80
 	
 	=================================
 	
@@ -138,76 +139,76 @@
 	=======================
 	
 	mov    ebx,0x8049b12
-    xor    ecx,ecx
-    lea    eax, [ecx+0x05]
-    cdq
-    int    0x80
+    	xor    ecx,ecx
+    	lea    eax, [ecx+0x05]
+    	cdq
+    	int    0x80
 
-    mov    ecx ,ebx
-    mov    ebx,eax
-    lea    eax, [edx+0x03]
-    mov    dl,0x1e
-    int    0x80
+    	mov    ecx ,ebx
+    	mov    ebx,eax
+    	lea    eax, [edx+0x03]
+    	mov    dl,0x1e
+    	int    0x80
 
 
-    lea    eax, [edx - 0x1A]
-    lea    ebx, [eax-0x03]
-    int    0x80 
+    	lea    eax, [edx - 0x1A]
+    	lea    ebx, [eax-0x03]
+    	int    0x80 
 	
 	========================
 	
 	mov    ebx,0x8049b12
-    cdq
+    	cdq
 	lea    eax, [edx+0x05]
-    int    0x80
+    	int    0x80
 
-    xchg   ebx, eax
-    mov    ecx, eax
-    lea    eax, [edx+0x03]
-    mov    dl,0x1e
-    int    0x80
+    	xchg   ebx, eax
+    	mov    ecx, eax
+    	lea    eax, [edx+0x03]
+    	mov    dl,0x1e
+    	int    0x80
 
-    lea    eax, [edx - 0x1A]
-    lea    ebx, [eax-0x03]
-    int    0x80 
+    	lea    eax, [edx - 0x1A]
+    	lea    ebx, [eax-0x03]
+    	int    0x80 
 	
 	========================
 	
 	
 	mov    ebx,0x8049b12
-    xor    ecx, ecx
-    lea    eax, [ecx + 0x5]
-    cdq
-    int    0x80
+    	xor    ecx, ecx
+    	lea    eax, [ecx + 0x5]
+    	cdq
+    	int    0x80
 
-    xchg   ebx, eax
-    mov    ecx, eax
-    lea    eax, [edx+0x03]
-    mov    dl,0x1e
-    int    0x80
+   	xchg   ebx, eax
+   	mov    ecx, eax
+   	lea    eax, [edx+0x03]
+ 	mov    dl,0x1e
+   	int    0x80
 
 
-    lea    eax, [edx - 0x1A]
-    lea    ebx, [eax-0x03]
-    int    0x80 
+    	lea    eax, [edx - 0x1A]
+    	lea    ebx, [eax-0x03]
+    	int    0x80 
 	
 	========================
 	
 	;29 Byte Solution
 	
-	mov    ebx,0x8049b12		; move 0x8049b12 into ebx
-    xor    ecx, ecx				; zero ecx
-    lea    eax, [ecx + 0x5]		; mov 5 into eax
-    cdq							; zero edx
-    int    0x80					; syscall
+	mov    ebx,0x8049b12			; move 0x8049b12 into ebx
+    	xor    ecx, ecx				; zero ecx
+    	lea    eax, [ecx + 0x5]			; mov 5 into eax
+    	cdq					; zero edx
+    	int    0x80				; syscall
 
-    xchg   ebx, eax				; exchange ebx and eax
-    mov    ecx, eax				; eax into ecx
-    lea    eax, [edx+0x03]		; 3 into eax
-    mov    dl,0x1e				; 30?(0x1e) into edx(dl)
-    int    0x80					; syscall
+    	xchg   ebx, eax				; exchange ebx and eax
+    	mov    ecx, eax				; eax into ecx
+    	lea    eax, [edx+0x03]			; 3 into eax
+    	mov    dl,0x1e				; 30?(0x1e) into edx(dl)
+    	int    0x80				; syscall
 
-    mov    eax, 0x04			; 4 into eax
-    mov    ebx, 0x01			; 1 into ebx
-    int    0x80					; syscall
+    	mov    eax, 0x04			; 4 into eax
+    	mov    ebx, 0x01			; 1 into ebx
+    	int    0x80				; syscall
 	
